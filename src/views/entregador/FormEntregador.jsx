c7d63c127c0d4ed5b7b2cfbff9dce97006f579dd
aluno@aluno <aluno@ifpe.com>
2023-03-30 20:51:05 -0300

    aula30

---
+++ src/views/entregador/FormEntregador.jsx
@@ -0,0 +1,193 @@
+import React from "react";
+import InputMask from 'react-input-mask';
+import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
+
+class FormEntregador extends React.Component{
+
+    render(){
+        return(
+            <div>
+
+                <div style={{marginTop: '3%'}}>
+
+                    <Container textAlign='justified' >
+
+                        <h2> <span style={{color: 'darkgray'}}> Entregador &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro </h2>
+
+                        <Divider />
+
+						<div style={{marginTop: '4%'}}>
+
+							<Form>
+
+								<Form.Group widths='equal'>
+
+									<Form.Input
+										required
+										fluid
+										label='Nome'
+										maxLength="100"
+									/>
+
+									<Form.Input
+										fluid
+										label='CPF'>
+										<InputMask 
+										mask="999.999.999-99"/> 
+									</Form.Input>
+
+                                    <Form.Input
+										fluid
+										label='RG'>
+										<InputMask 
+										mask="99.999.999"/> 
+									</Form.Input>
+
+
+								</Form.Group>
+								
+								<Form.Group>
+
+                                <Form.Input
+                                        fluid
+                                        label='DT Nascimento'
+                                        width={6}
+                                    >
+                                        <InputMask 
+                                            mask="99/99/9999" 
+                                            maskChar={null}
+                                            placeholder="Ex: 20/03/1985"
+                                        /> 
+                                    </Form.Input>
+
+									<Form.Input
+										fluid
+                                        required
+										label='Fone Celular'
+                                        width={6}>
+										<InputMask 
+										mask="(99) 9999.9999" /> 
+									</Form.Input>
+
+									<Form.Input
+										fluid
+										label='Fone Fixo'
+                                        width={6}>
+										<InputMask 
+										mask="(99) 9999.9999" /> 
+									</Form.Input>
+
+                                    <Form.Input
+										fluid
+										label='QTD Entregas Realizadas'
+                                        width={6}>
+										<InputMask 
+										mask="999"/> 
+									</Form.Input>
+
+                                    <Form.Input
+										fluid
+										label='Valor Por Frete'
+                                        width={6}>
+										<InputMask 
+										mask="99,99$"/> 
+									</Form.Input>
+                                   
+								</Form.Group>
+
+                                <Form.Group widths='equal'>
+
+									<Form.Input
+										fluid
+										label='Bairro'
+										maxLength="100"
+									/>
+
+                                    <Form.Input
+										fluid
+										label='Cidade'
+										maxLength="100"
+									/>
+
+                                    <Form.Input
+										fluid
+										label='CPF'>
+										<InputMask 
+										mask="999.999.999-99"/> 
+									</Form.Input>
+
+                                </Form.Group>
+
+                                <Form.Group widths='equal'>
+
+                                <Form.Input
+										fluid
+										label='UF'
+										maxLength="100"
+									/>   
+                                </Form.Group>
+
+                                <Form.Group widths='equal'>
+
+                                <Form.Input
+										fluid
+										label='UF'
+                                        placeholder="Selecione"
+                                        InputMessage
+										maxLength="100"
+									/>   
+                                </Form.Group>
+
+                                <Form.Group widths='equal'>
+
+                                <Form.Input
+										fluid
+										label='Complemente'
+										maxLength="100"
+									/>   
+                                </Form.Group>
+
+								<Form.Group widths='equal' style={{marginTop: '4%'}}  className='form--empresa-salvar'>
+
+									<Button
+										type="button"
+										inverted
+										circular
+										icon
+										labelPosition='left'
+										color='orange'
+										onClick={this.listar}
+										>
+										<Icon name='reply' />
+										Voltar
+									</Button>
+
+									<Container textAlign='right'>
+										
+										<Button
+											inverted
+											circular
+											icon
+											labelPosition='left'
+											color='blue'
+											floated='right'
+											onClick={this.salvar}
+										>
+											<Icon name='save' />
+											Salvar
+										</Button>
+										
+									</Container>
+
+								</Form.Group>
+
+							</Form>
+						</div>
+                    </Container>
+                </div>
+			</div>
+		)
+	}
+}
+
+export default FormEntregador;
---
+++ src/views/produto/FormProduto.jsx
@@ -0,0 +1,128 @@
+import React from "react";
+import InputMask from 'react-input-mask';
+import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
+
+class FormProduto extends React.Component{
+
+    render(){
+        return(
+            <div>
+
+                <div style={{marginTop: '3%'}}>
+
+                    <Container textAlign='justified' >
+
+                        <h2> <span style={{color: 'darkgray'}}> Produto &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro </h2>
+
+                        <Divider />
+
+						<div style={{marginTop: '4%'}}>
+
+							<Form>
+
+								<Form.Group widths='equal'>
+
+									<Form.Input
+										required
+										fluid
+										label='Titulo'
+                                        placeholder="Informe o titulo do produto"
+										maxLength="100"
+                                        size= "100"
+									/>
+
+									<Form.Input
+                                        required
+										fluid
+										label='Codigo de Produto'>
+										<InputMask 
+                                        placeholder="Informe o codigo do produto"
+										mask="999999999"/> 
+									</Form.Input>
+
+								</Form.Group>
+								
+                                <Form.Input
+										fluid
+										label='Descriçaõ'
+                                        placeholder="Informe a descrição do produto"
+                                        textarea= "tet"
+										maxLength="default"
+									/>
+
+								<Form.Group>
+
+                                <Form.Input
+                                        required
+										fluid
+										label='Valor Unitario'
+                                        width={6}>
+										<InputMask 
+										mask="999.99$"/> 
+									</Form.Input>
+
+
+									<Form.Input
+										fluid
+										label='Tempo de Entrega Minímo em Minutos'
+                                        width={6}>
+										<InputMask 
+                                        placeholder="31"
+										mask="99" /> 
+									</Form.Input>
+
+                                    <Form.Input
+										fluid
+										label='Tempo de Entrega Maximo em Minutos'
+                                        width={6}>
+										<InputMask 
+                                        placeholder="41"
+										mask="99" /> 
+									</Form.Input>
+
+								</Form.Group>
+
+								<Form.Group widths='equal' style={{marginTop: '4%'}}  className='form--empresa-salvar'>
+
+									<Button
+										type="button"
+										inverted
+										circular
+										icon
+										labelPosition='left'
+										color='orange'
+										onClick={this.listar}
+										>
+										<Icon name='reply' />
+										Voltar
+									</Button>
+
+									<Container textAlign='right'>
+										
+										<Button
+											inverted
+											circular
+											icon
+											labelPosition='left'
+											color='blue'
+											floated='right'
+											onClick={this.salvar}
+										>
+											<Icon name='save' />
+											Salvar
+										</Button>
+										
+									</Container>
+
+								</Form.Group>
+
+							</Form>
+						</div>
+                    </Container>
+                </div>
+			</div>
+		)
+	}
+}
+
+export default FormProduto;
